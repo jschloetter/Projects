30684544e16a45098f7efb0135eaac370d003089
Jeff Schloetter <jeffreyschloetter@gmail.com>
2023-10-08 20:17:04 -0700

    Initial Project Commit

---
+++ Portfolio Website/README.md
@@ -0,0 +1,13 @@
+# Portfolio Website
+
+This is the source code for my portfolio website.
+
+## Built with
+
+* HTML
+* CSS
+* JavaScript
+
+## Demo
+
+You can visit the website by going [insert github link]
--- RandomGenerator/TextGenerator.js
+++ RandomGenerator/TextGenerator.js
@@ -1,7 +1,20 @@
 // Life Assignment Generator
 // Tired of making choices? Try this!
-// Will give you a fulfilling career, a hobby, and what you should invest in.
+// This program will give you a fulfilling career, a hobby, and what you should invest in.
 // Example: Your career is: HVAC Technician. Your favorite hobby is: model airplanes. You should invest in: AAPL
 
-console.log('Test Message');
 
+let TextArray = {
+    careerChoices: ['Payroll Clerk', 'Energy Auditor', 'Financial Examiner', 'Telemarketer', 'Janitorial Supervisor', 'Neurodiagnositc Technologist', 'Entertainment Attendant', 'Earth Driller', 'Architectural Drafter'],
+    favoriteHobby: ['Morphology', 'Animal Fancy', 'Computer Programming', 'Simple Living', 'Amateur Radio', 'Dining', 'Stretching', 'Metalworking', 'Livestreaming', 'Patch Collecting'],
+    investIn: ['Kimberly-Clark Corporation', 'Hartford Financial Services', 'GFL Environmental', 'Southwestern Energy Company', 'AZEK Company Inc', 'YETI Holdings Inc', 'American Eagle Outfitters, Inc', 'Barnes & Nobles Education, Inc', 'Amplify Energy Corp.', 'Five Point Holdings']
+}
+
+const generateLifeAssignment = () => {
+    let i = Math.floor(Math.random() * 10);
+    let j = Math.floor(Math.random() * 10);
+    let k = Math.floor(Math.random() * 10);
+    return `Your career is: ${TextArray.careerChoices[i]}. Your favorite hobby is: ${TextArray.favoriteHobby[j]}. You should invest in: ${TextArray.investIn[k]}`;
+}
+
+console.log(generateLifeAssignment());
